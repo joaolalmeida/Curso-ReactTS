92e07a76ebd8df75ccccd32bc7b2b0085989860a
joaolalmeida <joaolalmeida20@gmail.com>
2023-12-07 14:24:18 -0300

    feat: add form

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,16 +1,30 @@
-import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 export const Login = () => {
-  const history = useNavigate()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-  const handleClick = () => {
-    history('pagina-inicial')
+  const handleEntrar = () => {
+    console.log(email)
+    console.log(setPassword)
   }
 
   return (
-    <div>
-      Login
-      <button onClick={handleClick}>Pagina inicial</button>
+  <div>
+    <form>
+      <label>
+        <span>Email</span>
+        <input value={email} onChange={e => setEmail(e.target.value)}/>
+      </label>
+      <label>
+        <span>Senha</span>
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+      </label>
+
+      <button type="button" onClick={handleEntrar}>
+        Entrar
+      </button>
+    </form>
     </div>
   )
 }
